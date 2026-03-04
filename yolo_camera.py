102ec33c669bb672247a3b8238b4e40d70280905
contrerascastroale-ui <contrerascastroale@gmail.com>
2026-03-04 18:00:49 -0500

    Toma de videos y fps-report de las resoluciones

--- yolo_camera.py
+++ yolo_camera.py
@@ -147,6 +147,6 @@ if __name__ == "__main__":
     try:
         # imgsz define tanto la resolución de la cámara como la de la IA
         # Valores recomendados: 320, 640, 1280
-        stream_yolo(show_fps=True, imgsz=640)
+        stream_yolo(show_fps=True, imgsz=1280)
     except Exception as e:
         print(f"\n[ERROR] Ocurrió un problema: {e}")
